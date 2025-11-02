a62920498c045de0b097745c3047ab2460a057d8
Allen <allenretuta10@yahoo.com>
2025-11-02 11:26:51 +0800

    Hero Section

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,7 @@ import { HeroSection } from "../sections/HeroSection"
 
 export const MainPage = () => {
     return (
-        <div className="bg-background min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col box-border container mx-auto px-4">
             {/* Hero */}
                 <HeroSection /> 
             {/* About Me */}
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,11 +1,38 @@
+import { SiGmail, SiInstagram, SiLinkedin } from "react-icons/si"
+import { Switch } from "@/components/ui/switch";
+const links = [
+    { name: 'About Me', href: '#about' },
+    { name: 'Projects', href: '#projects' },
+    { name: 'Contact Me', href: '#contact' }
+];
+
 export const HeroSection = () => {
     return (
-        <section className="min-h-screen flex justify-center items-center" >
-            
+        <section className="min-h-screen flex flex-col justify-between items-center py-10" >
+
             {/* NAVIGATION BAR */}
-            <div className="flex justify-between">
+            <div className="flex justify-between items-center w-full">
+                {/* CONTACT ICONS */}
+                <div className="flex space-x-2">
+                    <SiLinkedin />
+                    <SiGmail />
+                    <SiInstagram />
+                </div>
+
+                {/* PAGE LINKS */}
+                <div className="flex justify-between space-x-10">
+                    {links.map(link => (
+                        <a
+                            key={link.name}
+                            href={link.href}>
+                            {link.name}
+                        </a>
+                    ))}
+                </div>
+
+                {/* THEME TOGGLE */}
                 <div>
-                    
+                    <Switch defaultChecked />
                 </div>
             </div>
 
@@ -21,6 +48,9 @@ export const HeroSection = () => {
             </div>
 
             {/* SCROLL DOWN ARROW */}
+            <div>
+
+            </div>
         </section>
     )
 }
