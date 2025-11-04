0b67359e1a2f149acc775a20ff1c74dd3f857bfa
Allen <allenretuta10@yahoo.com>
2025-11-04 10:10:31 +0800

    do project section

--- src/sections/BadgeSection.jsx
+++ src/sections/BadgeSection.jsx
@@ -8,8 +8,8 @@ export const BadgeSection = () => {
             <div className="flex w-3xl gap-20 flex-wrap" >
                 {badgesData.map((badge, idx) => (
                     <span key={idx} className="flex flex-col flex-1 items-center justify-between min-w-40 gap-2">
-                        <img src={badge.img} alt="Badge Logo" className="max-w-1xl" />
-                        <span className="text-center flex flex-col items-center h-1/2">
+                        <img src={badge.img} alt="Badge Logo" className="h-50" />
+                        <span className="text-center flex flex-col items-center h-20">
                             <p>{badge.title}</p>
                             <p className="text-highlight" >{badge.provider}</p>
                             <p>{badge.date}</p>
